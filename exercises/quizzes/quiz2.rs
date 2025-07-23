6a3373b2765264d105228bcbd7db8c477e500eab
Quentin Burgess <qutn.burgess@gmail.com>
2025-07-23 16:51:37 +0200

    feat(quizz): done 2nd one, iterator solution looks sexy but scary ATM

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -24,10 +24,26 @@ enum Command {
 }
 
 mod my_module {
+    //use std::process::Output;
+
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> { 
+        let mut output = vec![];
+
+        for (string, command) in input {
+            output.push(match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append (usize) =>  string + &"bar".repeat(usize),
+            })
+
+            
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +55,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
